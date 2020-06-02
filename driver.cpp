ca6f3a3251a66774cbc2e66637aeaf1f81bdcd0b
theorangelego <gzjosef@gmail.com>
2020-06-02 14:28:24 -0700

    driver: unformat returned values for representation

--- driver.cpp
+++ driver.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 
-extern "C" int kubic_main( void );
+const unsigned long long int FALSE = 0x7FFFFFFFFFFFFFFF;
+const unsigned long long int TRUE  = 0xFFFFFFFFFFFFFFFF;
 
-void error( unsigned int _errorCode ) {
+extern "C" unsigned long long int kubic_main( void );
+
+void error( long long int _errorCode ) {
   exit( 1 );
 }
 
+std::string unformatValue( unsigned long long int _value ) {
+  if ( _value == FALSE ) {
+    return "false";
+  } else if ( _value == TRUE ) {
+    return "true";
+  } else if ( ( _value & 0x1 ) == 0x0 ) {
+    return std::to_string( _value >> 1 );
+  }
+
+  return "";
+}
 int main( void ) {
-  int kubicResult = kubic_main();
+  unsigned long long int kubicResult = kubic_main();
 
   // if ( kubicResult ) error( kubicResult );
 
-  std::cout << "Kubic returned :: " << kubicResult << std::endl;
+  std::cout << "Kubic returned :: " << unformatValue( kubicResult ) << std::endl;
 
   return 0;
 }
